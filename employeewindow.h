967d2734f09f35a3f2f82891c8c165875704d7fe
unknown <20119372@student.hcmute.edu.vn>
2023-11-25 04:55:24 +0700

    Give to head after #include

--- employeewindow.h
+++ employeewindow.h
@@ -1,5 +1,18 @@
 #ifndef EMPLOYEEWINDOW_H
 #define EMPLOYEEWINDOW_H
+
+#include <QMainWindow>
+#include "staff.h"
+#include "QScreen"
+#include <QCloseEvent>
+#include "staff.h"
+#include "table.h"
+#include "iostream"
+#include "fstream"
+#include "sstream"
+#include "string"
+#include <QPushButton>
+
 #define MAKE_TABLE(w, h, num, row, column, size, w_table, h_table)\
 for (int j = 0; j < column; j++) {\
         if (j < column/2) {\
@@ -81,17 +94,6 @@ for (int j = 0; j < column; j++) {\
     } \
 }
 
-#include <QMainWindow>
-#include "staff.h"
-#include "QScreen"
-#include <QCloseEvent>
-#include "staff.h"
-#include "table.h"
-#include "iostream"
-#include "fstream"
-#include "sstream"
-#include "string"
-#include <QPushButton>
 namespace Ui {
 class employeeWindow;
 }
